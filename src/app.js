1d27fd62042df928f94e704dbca4616c954c39d2
Eric Son <ym.eric.son@gmail.com>
2021-02-25 02:15:42 -0600

    displaying school info right of the map
    circle size adjusted based on enrollment size

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ import './main.css';
 
 console.log(cps)
 
-var margin = {top: 100, bottom: 50, side: 50};
-var width = 900;
+var margin = {top: 50, bottom: 30, side: 30};
+var width = 500;
 var height = 800;
 
 var projection = d3.geoMercator();
@@ -16,11 +16,14 @@ var projection = d3.geoMercator();
 // set boundaries
 var svg = d3.select(".container-fluid")
     .append("svg")
+        .attr('class', 'map')
         .attr("width", width + (2 * margin.side))
         .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform",
-            "translate(" + margin.side + "," + margin.top + ")");
+        
+    // .append("g")
+    //     .attr("transform",
+    //         "translate(" + margin.side + "," + margin.top + ")")
+            ;
 
 //Load in GeoJSON data
 d3.json("./data/chicago_map.geojson").then(function(data) {
@@ -41,26 +44,43 @@ d3.json("./data/chicago_map.geojson").then(function(data) {
     .style("stroke", "black")
     .style("opacity", .3)
 
+  // tooltip to display school info
   var Tooltip = d3.select(".container-fluid")
     .append("svg")
+    .append("foreignObject")
     .attr("class", "tooltip")
+  
+  var size = d3.scaleLinear()
+    .domain([48, 4500])  // What's in the data
+    .range([ 5, 30])  // Size in pixel
 
   // actions for when hovering over a circle
   var mouseover = function(event, d) {
+    Tooltip
+      .style("opacity", 0.9)
+      .html("<h3>" + d.school_name_long + "</h3>" +
+            "School Type: " + d.school_type + "<br/>" +
+            "Address: " + d.address + "<br/>" +
+            "Phone #: " + d.phone 
+            )
     d3.select(this)
       .style("stroke", "red")
       .attr("stroke-width", 3)
   }
-  var mousemove = function(event, d) {
-    Tooltip
-      .style("opacity", 0.9)
-      .html("<p>School: " + d.school_name_long + "</p>")
-      .style("left", (event.clientX + 30) + "px")
-      .style("top", (event.clientY - 50) + "px")
-  }
+  // var mousemove = function(event, d) {
+  //   Tooltip
+  //     .style("opacity", 0.9)
+  //     .html("<strong>" + d.school_name_long + "</strong>" + "<br/>" +
+  //           "School Type: " + d.school_type + "<br/>" +
+  //           "Address: " + d.address + "<br/>" +
+  //           "Phone #: " + d.phone 
+  //           )
+  //     .style("left", (event.clientX + 30) + "px")
+  //     .style("top", (event.clientY - 50) + "px")
+  // }
   var mouseleave = function(d) {
     Tooltip
-      .style("opacity", 0)
+      .style("opacity", 0.1)
     d3.select(this)
       .style("stroke", "green")
       .attr("stroke-width", 1)
@@ -78,14 +98,14 @@ d3.json("./data/chicago_map.geojson").then(function(data) {
   .append("circle")
     .attr("cx", function(d){ return projection([d.longitude, d.latitude])[0] })
     .attr("cy", function(d){ return projection([d.longitude, d.latitude])[1] })
-    .attr("r", 10)
+    .attr("r", function(d){ return size(d.enrollment) })
     .style("fill", "69b3a2")
     .attr("stroke", "green")
     .attr("stroke-width", 1)
     .attr("fill-opacity", .4) 
   .on("mouseover", mouseover)
-  .on('mousemove', mousemove)
   .on("mouseleave", mouseleave)
+  
 })
 
 
